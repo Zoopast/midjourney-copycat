8f32ddfbc01993f858c81d91f4c39ba1ea5c22f2
Gabriel San Martín <gsandaur@hotmail.com>
2023-05-17 12:39:21 -0400

    style: add background color

--- src/App.vue
+++ src/App.vue
@@ -5,3 +5,9 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <RouterView />
 </template>
+<style>
+  body, html {
+    --tw-bg-opacity: 1;
+    background-color: rgb(8 11 22/var(--tw-bg-opacity));
+  }
+</style>
