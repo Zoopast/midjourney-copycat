13be21974887bb75a2c8884d0cd3615d782802e1
Gabriel San Martín <gsandaur@hotmail.com>
2023-05-17 14:52:59 -0400

    feat: add home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
+  import SideBar from "@/components/SideBarComponent.vue";
+  import ImagesShowcase from "@/components/home/ImagesShowcaseComponent.vue";
 </script>
 
 <template>
-  <main>
+  <main
+    class="flex min-h-screen w-full select-text justify-center text-gray-700 antialiased"
+  >
+    <SideBar />
+    <ImagesShowcase />
   </main>
 </template>
