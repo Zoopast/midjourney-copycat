610c4f6f3dc88b57fd5f2a019895fd7c22eb37b0
Gabriel San Martin <gsandaur@hotmail.com>
2023-05-23 08:15:11 -0400

    refactor: move images to its own file

---
+++ src/assets/images.ts
@@ -0,0 +1,85 @@
+interface Image {
+	src: string;
+	tag: string;
+	date: string;
+	prompt: string;
+	author: string;
+	avatar: string;
+}
+
+const images: Image[] = [
+	{
+		src: "https://res.cloudinary.com/zoopast/image/upload/v1684467313/pvgob5jnznuzvrvd2nic.jpg",
+		tag: "/upscale",
+		date: "9 months ago",
+		prompt: "portrait photo of an Asia old warrior chief, tribal panther make up, blue on red, side profile, looking away, serious",
+		author: "Gabriel San Martin",
+		avatar: "https://picsum.photos/seed/1/300/500"
+	},
+	{
+		src: "https://res.cloudinary.com/zoopast/image/upload/v1684467413/eassw26a9qiszzxqyn9g.jpg",
+		tag: "/future",
+		date: "10 months ago",
+		prompt: "Love is in the air",
+		avatar: "https://picsum.photos/seed/2/300/500",
+		author: "Akiles Bailo"
+	},
+	{
+		src: "https://res.cloudinary.com/zoopast/image/upload/v1684467583/jmlxirfto6mvtydtjpda.jpg",
+		tag: "/imagination",
+		date: "A year ago",
+		prompt: "Darth vader wearing a gucci armor",
+		avatar: "https://picsum.photos/seed/2/300/500",
+		author: "Akiles Bailo"
+	},
+	{
+		src: "https://res.cloudinary.com/zoopast/image/upload/v1684467313/pvgob5jnznuzvrvd2nic.jpg",
+		tag: "/upscale",
+		date: "9 months ago",
+		prompt: "portrait photo of an Asia old warrior chief, tribal panther make up, blue on red, side profile, looking away, serious",
+		author: "Gabriel San Martin",
+		avatar: "https://picsum.photos/seed/1/300/500"
+	},
+	{
+		src: "https://res.cloudinary.com/zoopast/image/upload/v1684467413/eassw26a9qiszzxqyn9g.jpg",
+		tag: "/future",
+		date: "10 months ago",
+		prompt: "Love is in the air",
+		avatar: "https://picsum.photos/seed/2/300/500",
+		author: "Akiles Bailo"
+	},
+	{
+		src: "https://res.cloudinary.com/zoopast/image/upload/v1684467583/jmlxirfto6mvtydtjpda.jpg",
+		tag: "/imagination",
+		date: "A year ago",
+		prompt: "Darth vader wearing a gucci armor",
+		avatar: "https://picsum.photos/seed/2/300/500",
+		author: "Akiles Bailo"
+	},
+	{
+		src: "https://res.cloudinary.com/zoopast/image/upload/v1684467313/pvgob5jnznuzvrvd2nic.jpg",
+		tag: "/upscale",
+		date: "9 months ago",
+		prompt: "portrait photo of an Asia old warrior chief, tribal panther make up, blue on red, side profile, looking away, serious",
+		author: "Gabriel San Martin",
+		avatar: "https://picsum.photos/seed/1/300/500"
+	},
+	{
+		src: "https://res.cloudinary.com/zoopast/image/upload/v1684467413/eassw26a9qiszzxqyn9g.jpg",
+		tag: "/future",
+		date: "10 months ago",
+		prompt: "Love is in the air",
+		avatar: "https://picsum.photos/seed/2/300/500",
+		author: "Akiles Bailo"
+	},
+	{
+		src: "https://res.cloudinary.com/zoopast/image/upload/v1684467583/jmlxirfto6mvtydtjpda.jpg",
+		tag: "/imagination",
+		date: "A year ago",
+		prompt: "Darth vader wearing a gucci armor",
+		avatar: "https://picsum.photos/seed/2/300/500",
+		author: "Akiles Bailo"
+	},
+];
+
+export default images;
